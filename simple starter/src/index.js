383a87596b449e7bffd0063e0abf6979e71483ad
Unknown <boczelijah@gmail.com>
2018-04-17 21:22:23 -0400

    added more components
    updated search bar, added video list and video list items

--- simple starter/src/index.js
+++ simple starter/src/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-import SearchBar from './components/search_bar.js'
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar.js';
+import VideoList from './components/video_list.js';
 const API_KEY = 'AIzaSyA1JTXJntPh3jXdSWh5hv-10wlW8XusEQI';
+
+
 
 // Create a new component.
 // This component should produce some HTML
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			// this.setState({ videos: videos}) is equivalent to this ES6 refractor
+			this.setState({ videos });
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 
