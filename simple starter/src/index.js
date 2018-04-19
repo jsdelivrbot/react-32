ccc8201ade89aae4be4406c51cd4a7e913358a71
Unknown <boczelijah@gmail.com>
2018-04-18 23:09:45 -0400

    added debounce to search bar

--- simple starter/src/index.js
+++ simple starter/src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -6,8 +7,6 @@ import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyA1JTXJntPh3jXdSWh5hv-10wlW8XusEQI';
-
-
 
 // Create a new component.
 // This component should produce some HTML
@@ -34,9 +33,11 @@ class App extends Component {
 	}
 
 	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
 		return (
 			<div>
-				<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+				<SearchBar onSearchTermChange={videoSearch}/>
 				<VideoDetail video={this.state.selectedVideo}/>
 				<VideoList 
 					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
