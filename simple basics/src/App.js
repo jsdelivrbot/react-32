4dbc9aa5c0c0eeb4633e034fba77e4248c803d0c
Unknown <boczelijah@gmail.com>
2018-05-17 21:02:25 -0400

    state changes with button click

--- simple basics/src/App.js
+++ simple basics/src/App.js
@@ -12,7 +12,15 @@ class App extends Component {
   }
 
   switchNameHandler = () => {
-    console.log('Was clicked');
+    // console.log('Was clicked');
+    // DON'T DO THIS: this.state.persons[1].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28},
+        { name: 'Manu', age: 29},
+        { name: 'Stephanie', age: 27}
+      ] 
+    })
   }
 
   render() {
@@ -21,7 +29,7 @@ class App extends Component {
         <h1>Hi I'm a React app</h1>
         <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age ={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age ={this.state.persons[1].age}/>
+        <Person name={this.state.persons[1].name} age ={this.state.persons[1].age}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age ={this.state.persons[2].age}/>
       </div>
     );
