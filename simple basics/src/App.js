d3d92a4f4814e9272589707c0faeb400611aaece
Unknown <boczelijah@gmail.com>
2018-05-18 10:24:44 -0400

    added examples of passing a reference to a method established in App.js

--- simple basics/src/App.js
+++ simple basics/src/App.js
@@ -5,21 +5,21 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28},
-      { name: 'Manu', age: 29},
-      { name: 'Stephanie', age: 26}
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked');
     // DON'T DO THIS: this.state.persons[1].name = 'Maximilian';
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28},
-        { name: 'Manu', age: 29},
-        { name: 'Stephanie', age: 27}
-      ] 
+        { name: newName, age: 30 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
     })
   }
 
@@ -27,10 +27,18 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi I'm a React app</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age ={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age ={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age ={this.state.persons[2].age}/>
+        {/* This way of passing reference to method is less efficient than the bind way further down */}
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}>My Hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
