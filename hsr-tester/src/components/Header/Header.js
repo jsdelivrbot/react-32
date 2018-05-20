445675d26c5b6b7337521dec94151a1e3218abdd
Unknown <boczelijah@gmail.com>
2018-05-20 11:10:59 -0400

    working responsive header

--- hsr-tester/src/components/Header/Header.js
+++ hsr-tester/src/components/Header/Header.js
@@ -6,8 +6,27 @@ const header = () => {
     <header className="header">
       <img src={Logo} alt="logo" className="logo"/>
       <h1 className="logo-title">Human Sounds Records</h1>
+      <section className="header-list">
+      <ul>
+        <li>
+          <a >Home</a>
+        </li>
+        <li>
+          <a >Artists</a>
+        </li>
+        <li>
+          <a >Store</a>
+        </li>
+        <li>
+          <a >Bandcamp</a>
+        </li>
+        <li>
+          <a >About</a>
+        </li>
+      </ul>
+    </section>
     </header>
   )
 }
 
-export default header;
+export default header; 
