887bdab53d6bddbc7095a8f29c1f842517f9f9c3
Unknown <boczelijah@gmail.com>
2018-05-21 13:23:19 -0400

    replaced the conditional ternary operator with a more elegant JSX format

--- simple basics/src/App.js
+++ simple basics/src/App.js
@@ -37,8 +37,9 @@ class App extends Component {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState( { showPersons: !doesShow } );
+    this.setState({ showPersons: !doesShow });
   }
+  
   render() {
     const style = {
       backgroundColor: 'lightBlue',
@@ -48,28 +49,33 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div >
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
-        {/* This way of passing reference to method is less efficient than the bind further down */}
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {this.state.showPersons === true ?
-          <div >
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Max!')}
-              changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age} />
-          </div> : null
-        }
+          {persons}
       </div>
     );
   }
