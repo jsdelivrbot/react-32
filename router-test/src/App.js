64ff8dae693fc9628dbb2ae198e6860058b824ee
Unknown <boczelijah@gmail.com>
2018-06-17 20:26:50 -0400

    + test for react router from docs, reworked it a bit

---
+++ router-test/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import BasicExample from './components/BasicExample';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <BasicExample />
+      </div>
+    );
+  }
+}
+
+export default App;
